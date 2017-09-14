09b27be844463f604bdebad15ca40e23dc17ea71
Tijmen Brommet <tijmen@gmail.com>
2017-09-14 22:42:49 +0100

    Update search

--- lib/zoopla_search.rb
+++ lib/zoopla_search.rb
@@ -9,12 +9,12 @@ class ZooplaSearch
     include_rented: false,
     minimum_price: ((1400 * 12) / 52).to_i,
     maximum_price: ((1900 * 12) / 52).to_i,
-    minimum_beds: '2',
+    minimum_beds: '1',
   }
 
   SEARCHES = [
     {
-      area: 'Bethnal Green, London',
+      area: 'Tower Hamlets, London',
     },
     {
       area: 'Islington, London',
@@ -30,18 +30,7 @@ class ZooplaSearch
     SEARCHES.map do |search_query|
       JSON.parse(search_zoopla(search_query))['listing'].each do |source|
         source["area"] = search_query[:area].gsub(', London', '')
-        unless source['available_from_date']
-          puts "No `available_from_date`, skipping"
-          next
-        end
-
         source["from"] = Date.parse(source["available_from_date"])
-
-        unless source["from"] > Date.parse("2017-08-01") && source["from"] < Date.parse("2017-10-05")
-          puts "Not available in aug/sept: #{source["from"]}"
-          next
-        end
-
         source
       end
     end.flatten.compact
